bfa06335969e2921a2c199e5dd156e9187a4d6a1
Abdelrahman .Net <abdoelsimy123@gmail.com>
2021-12-25 23:20:59 +0200

    Add project files.

---
+++ SigChat/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SigChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SigChat.Controllers
+{
+    [Authorize]
+    public class ChatController : Controller
+    {
+        private readonly ILogger<ChatController> _logger;
+
+        public ChatController(ILogger<ChatController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SigChat/Models/ChatUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace SigChat.Models
+{
+    public class ChatUser: IdentityUser
+    {
+        public virtual ICollection<Message> Messages { get; set; }
+    }
+}
